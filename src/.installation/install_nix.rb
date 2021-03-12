dd891efe6e6aaa5b5adbbd51e311f9fa369f7cca
brett <mcdonalds.burger.dev@gmail.com>
2021-03-12 01:09:48 -0500

    update installation script

--- src/.installation/install_nix.rb
+++ src/.installation/install_nix.rb
@@ -38,7 +38,7 @@ if(File.exist?("/usr/local/bin/fpm"))
       puts "Downloading..."
       system("curl #{live}/.bin/fpm2_nix -o fpm")
       system("curl #{live}/src/.fpm_glasses.rb -o /usr/local/bin/.fpm_glasses.rb")
-      system("curl #{live}/src/.fpm_spine_install.py -o /usr/local/bin/.fpm_spine_install.py")
+      system("curl #{live}/src/.fpm_spine_install.rb -o /usr/local/bin/.fpm_spine_install.rb")
       system("curl #{live}/src/.fpm_spine_uninstall.py -o /usr/local/bin/.fpm_spine_uninstall.py")
       system("cp fpm /usr/local/bin/fpm")
     end
