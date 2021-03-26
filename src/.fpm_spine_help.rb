60ea754e273c1e51e3ee5a5ed450bc16cb15f6aa
notronaldmcdonald <mcdonalds.burger.dev@gmail.com>
2021-03-26 16:18:37 -0400

    update install script

--- src/.fpm_spine_help.rb
+++ src/.fpm_spine_help.rb
@@ -12,5 +12,5 @@ helpfile_needed = gets
 helpfile_needed = helpfile_needed.chomp
 puts "Looking up #{helpfile_needed} on remote..."
 system("curl -s https://raw.githubusercontent.com/notronaldmcdonald/fpm/core/pkgs/help/#{helpfile_needed}")
-puts "If nothing printed, or an error message printed, the package does not have any help documentation."
+puts "\nIf nothing printed, or an error message printed, the package does not have any help documentation."
 puts "Passing the ball back..."
